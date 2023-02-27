7812af130dedd9d5299fd0a76c95118aa98c9c84
Luigi213 <reyesgianluigi88@gmail.com>
2023-02-27 16:37:15 +0100

    close #8

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                SONO IL FOOTER
+            </div>
+        </div>
+    </div>
+</footer>
