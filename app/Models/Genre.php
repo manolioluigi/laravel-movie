30886d175516afa5325690cf8c9cff822efe64d9
manolioluigi <luigi.manolio1999@gmail.com>
2023-03-07 15:27:41 +0100

    completamento issue

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Movie;
 
 class Genre extends Model
 {
     use HasFactory;
+    protected $fillable = ['genre', 'slug'];
+
+    public function movies(){
+        return $this->hasMany(Movie::class);
+    }
 }
