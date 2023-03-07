2b2f666c49a678e3e72dca0cb0eeea8f5efb8429
giuseppemaisto <gmaisto98@gmail.com>
2023-03-07 15:23:42 +0100

    fatta migraation

--- database/migrations/2023_03_07_141052_create_genres_table.php
+++ database/migrations/2023_03_07_141052_create_genres_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('genres', function (Blueprint $table) {
             $table->id();
+            $table->string('genre', 50)->unique();
+            $table->string('slug', 50);
             $table->timestamps();
         });
     }
