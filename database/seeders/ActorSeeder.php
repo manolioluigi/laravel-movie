e596d5448a73473325784c2afec0c95e1b399d13
Luigi213 <reyesgianluigi88@gmail.com>
2023-03-07 16:46:53 +0100

    close #52

--- database/seeders/ActorSeeder.php
+++ database/seeders/ActorSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Actor;
 
 class ActorSeeder extends Seeder
 {
@@ -14,6 +15,15 @@ class ActorSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $actors = config('actors');
+
+        foreach($actors as $actor){
+            $newActor = new Actor();
+
+            $newActor->name = $actor;
+            $newActor->slug = Actor::generateSlug($actor);
+
+            $newActor->save();
+        }
     }
 }
